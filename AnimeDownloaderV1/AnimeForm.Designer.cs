4764801bff9d1234ee058febeb73fc6531319b99
karl.spiteri <karl.spiteri.e21724@mcast.edu.mt>
2022-09-14 14:35:21 +0200

    Commit

---
+++ AnimeDownloaderV1/AnimeForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace VideoDownloaderV1
+{
+    partial class AnimeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnimeForm));
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.AnimeLink_TextBox = new System.Windows.Forms.TextBox();
+            this.AnimeLink_Label = new System.Windows.Forms.Label();
+            this.Start_Label = new System.Windows.Forms.Label();
+            this.Start_TextBox = new System.Windows.Forms.TextBox();
+            this.End_TextBox = new System.Windows.Forms.TextBox();
+            this.End_Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Location = new System.Drawing.Point(603, 6);
+            this.Start_Button.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(104, 94);
+            this.Start_Button.TabIndex = 0;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // AnimeLink_TextBox
+            // 
+            this.AnimeLink_TextBox.Location = new System.Drawing.Point(193, 6);
+            this.AnimeLink_TextBox.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.AnimeLink_TextBox.Name = "AnimeLink_TextBox";
+            this.AnimeLink_TextBox.Size = new System.Drawing.Size(394, 40);
+            this.AnimeLink_TextBox.TabIndex = 1;
+            // 
+            // AnimeLink_Label
+            // 
+            this.AnimeLink_Label.AutoSize = true;
+            this.AnimeLink_Label.Location = new System.Drawing.Point(17, 9);
+            this.AnimeLink_Label.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.AnimeLink_Label.Name = "AnimeLink_Label";
+            this.AnimeLink_Label.Size = new System.Drawing.Size(160, 33);
+            this.AnimeLink_Label.TabIndex = 2;
+            this.AnimeLink_Label.Text = "Anime Link";
+            // 
+            // Start_Label
+            // 
+            this.Start_Label.AutoSize = true;
+            this.Start_Label.Location = new System.Drawing.Point(17, 63);
+            this.Start_Label.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.Start_Label.Name = "Start_Label";
+            this.Start_Label.Size = new System.Drawing.Size(158, 33);
+            this.Start_Label.TabIndex = 3;
+            this.Start_Label.Text = "Starting Ep";
+            // 
+            // Start_TextBox
+            // 
+            this.Start_TextBox.Location = new System.Drawing.Point(193, 60);
+            this.Start_TextBox.Name = "Start_TextBox";
+            this.Start_TextBox.Size = new System.Drawing.Size(100, 40);
+            this.Start_TextBox.TabIndex = 4;
+            // 
+            // End_TextBox
+            // 
+            this.End_TextBox.Location = new System.Drawing.Point(487, 60);
+            this.End_TextBox.Name = "End_TextBox";
+            this.End_TextBox.Size = new System.Drawing.Size(100, 40);
+            this.End_TextBox.TabIndex = 5;
+            // 
+            // End_Label
+            // 
+            this.End_Label.AutoSize = true;
+            this.End_Label.Location = new System.Drawing.Point(328, 63);
+            this.End_Label.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.End_Label.Name = "End_Label";
+            this.End_Label.Size = new System.Drawing.Size(148, 33);
+            this.End_Label.TabIndex = 6;
+            this.End_Label.Text = "Ending Ep";
+            // 
+            // AnimeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 115);
+            this.Controls.Add(this.End_Label);
+            this.Controls.Add(this.End_TextBox);
+            this.Controls.Add(this.Start_TextBox);
+            this.Controls.Add(this.Start_Label);
+            this.Controls.Add(this.AnimeLink_Label);
+            this.Controls.Add(this.AnimeLink_TextBox);
+            this.Controls.Add(this.Start_Button);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.MaximizeBox = false;
+            this.Name = "AnimeForm";
+            this.Text = "Anime Downloader";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AnimeForm_FormClosed);
+            this.Load += new System.EventHandler(this.AnimeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.TextBox AnimeLink_TextBox;
+        private System.Windows.Forms.Label AnimeLink_Label;
+        private System.Windows.Forms.Label Start_Label;
+        private System.Windows.Forms.TextBox Start_TextBox;
+        private System.Windows.Forms.TextBox End_TextBox;
+        private System.Windows.Forms.Label End_Label;
+    }
+}
+
---
+++ AnimeDownloaderV1/AnimeForm.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using OpenQA.Selenium.Support.Extensions;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using AngleSharp.Dom;
+using System.Security.Policy;
+using OpenQA.Selenium.Firefox;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace VideoDownloaderV1
+{
+    public partial class AnimeForm : Form
+    {
+        /// <summary>
+        /// make try and catch so that if an error occurs will skip the episode and continue to next one       DONE TO MANNY FAILURES
+        /// HAVE TO MAKE all in one window so as to see all downloads via download manager 
+        /// </summary>                               
+
+        bool fail = false;
+        SmtpClient Client;
+        IWebDriver driver;
+        string emailS = "karl.methode.spiteri@gmail.com";
+        string emailR = "karl.spiteri777@gmail.com";
+
+        List<string> failList = new List<string>();
+
+        public AnimeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimeForm_Load(object sender, EventArgs e)
+        {               
+            AnimeLink_TextBox.Text = "https://9anime.tube/watch/detective-conan/";
+            Start_TextBox.Text = "1002";
+            End_TextBox.Text = "1057";
+        }
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            int end = Int32.Parse(End_TextBox.Text);
+            int start = Int32.Parse(Start_TextBox.Text);
+
+            for (int i = start; i <= end; i++)
+            {
+                openEdge(AnimeLink_TextBox.Text + "?ep=" + i.ToString());
+                Thread.Sleep(5000);
+
+                if (AnimeLink_TextBox.Text.Contains("9anime"))
+                {
+                    try   //so as to not stop
+                    {
+                        download9anime();
+                    }
+                    catch (Exception) //Will keep count of failed episodes
+                    {
+                        failList.Add("Episode: " + i.ToString());
+                    }                    
+                }                                            
+
+                Thread.Sleep(10000); //wait a bit for the video to download
+            }
+
+            #region Sending Response Email
+            if (failList.Count > 0)
+            {
+                string failed = string.Join("\n", failList);
+                sendEmail("Downloader Finished", "Finished Downloading Episodes except the following: " + failed);
+            }
+            else
+            {
+                sendEmail("Downloader Finished", "Finished Downloading Episodes");
+            }
+            #endregion
+        }
+
+        private void chooseQuality()
+        {
+            try
+            {
+                edgeClick("//*[@id=\"container\"]/div/table/tbody/tr[3]/td[1]/a", 500);          //high res
+                fail = false;
+            }
+            catch (Exception)
+            {
+                fail = true;
+            }
+
+            if (fail == true)
+            {
+                try
+                {
+                    edgeClick("//*[@id=\"container\"]/div/table/tbody/tr[2]/td[1]/a", 500);
+                    fail = false;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void download9anime()
+        {
+            #region Play & Pause Video
+            edgeClick("//*[@id=\"player\"]", 3000);
+            edgeClick("//*[@id=\"player\"]", 0200);
+            edgeClick("//*[@id=\"player\"]", 0200);
+            edgeClick("//*[@id=\"player\"]", 0200); //to pause vid
+            #endregion
+
+
+            #region Switch Frame & Press Download
+            driver.SwitchTo().Frame("iframe-embed");
+            driver.SwitchTo().Frame("external-embed");
+            edgeClick("//*[@id=\"mediaplayer\"]/div[2]/div[13]/div[4]/div[2]/div[13]", 100);
+            #endregion
+
+
+            #region Going to process to download video
+            driver.SwitchTo().Window(driver.WindowHandles[1]); //seems reduntant but needed
+
+            chooseQuality();
+
+            try
+            {
+                edgeClick("//*[@id=\"F1\"]/button", 1000);
+            }
+            catch (Exception)
+            {
+                fail = true;
+            }
+
+            if (fail == true) //might switch flase firtst then true
+            {
+                chooseQuality();
+            }
+            else
+            {
+                try
+                {
+                    edgeClick("//*[@id=\"F1\"]/button", 1000);
+                }
+                catch (Exception) { }
+            }
+
+            IList<IWebElement> elements = driver.FindElements(By.TagName("div"));
+            ReadOnlyCollection<IWebElement> link = elements[8].FindElements(By.TagName("a"));
+
+            driver.Navigate().GoToUrl(link[0].GetAttribute("href"));
+            #endregion
+
+            #region Open NewTab & Close prev 2 tabs
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+
+            for (int i = 0; i < 2; i++)
+            {
+                driver.SwitchTo().Window(driver.WindowHandles.First());
+                driver.Close();
+            }
+            #endregion
+        }
+
+        private void edgeClick(string xPath, int delayMS)
+        {
+            driver.FindElement(By.XPath(xPath)).Click();
+            Thread.Sleep(delayMS);
+        }
+
+        private void openEdge(string URL)
+        {
+            var options = new EdgeOptions();
+
+            options.AddExtension(@"uBlock-Origin.crx");
+
+            new DriverManager().SetUpDriver(new EdgeConfig());
+            var chromeDriverService = EdgeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+
+            driver = new EdgeDriver(chromeDriverService, options);            
+            driver.Navigate().GoToUrl(URL); 
+        }
+
+        private void sendEmail(string subject, string msg)
+        {
+            #region SMTP Setup
+            Client = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential()
+                {
+                    UserName = emailS,
+                    Password = "yaiopydewhynacpm"
+                }
+            };
+            #endregion
+
+            #region Building Email
+            string Sender = "Downloader Bot";
+            string Reader = "Karl Spiteri";
+
+
+            MailAddress FromEmail = new MailAddress(emailS, Sender);
+            MailAddress ToEmail = new MailAddress(emailR, Reader);
+            MailMessage Message = new MailMessage()
+            {
+                From = FromEmail,
+                Subject = subject,
+                Body = msg,
+            };
+
+
+            Message.To.Add(ToEmail);
+            #endregion
+
+            Client.Send(Message);
+        }
+
+        private void AnimeForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+            catch (Exception) { }
+        }
+    }
+}
